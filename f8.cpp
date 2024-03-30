4f3802795f98edd89359a7840c2cecb6a85718ef
Prachiii04 <patelprachi041005@gmail.com>
2024-03-29 21:23:40 -0400

    checking for right arguments implemented

--- f8.cpp
+++ f8.cpp
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 #define kstringSize 21
 #define kfullPathSize 42
@@ -12,6 +13,26 @@ struct MyData
 
 int main(int argc, char* argv[])
 {
-	
+    if (argc != 5)
+    {
+        printf("Error");
+        return -1;
+    }
+
+    if ((strlen(argv[2]) > 20))
+    {
+        printf("Argument Error, Text is longer than 20 characters");
+        return -1;
+    }
+    else if (strlen(argv[3]))
+    {
+        printf("Argument Error, Destination path is longer than 20 character");
+        return -1;
+    }
+    else if ((strlen(argv[4]) > 20))
+    {
+        printf("Argument Error, File name is longer than 20 characters");
+        return -1;
+    }
 	return 0;
 }
