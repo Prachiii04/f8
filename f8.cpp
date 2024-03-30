ebbdf293fd52113d7277b54f15cf27c6da336d69
Prachiii04 <patelprachi041005@gmail.com>
2024-03-29 21:12:38 -0400

    declared struct

--- f8.cpp
+++ f8.cpp
@@ -1,7 +1,17 @@
 #include <stdio.h>
 
+#define kstringSize 21
+#define kfullPathSize 42
+struct MyData
+{
+	int howMany;
+	char theText[kstringSize];
+	char directoryPath[kstringSize];
+	char filename[kstringSize];
+};
+
 int main(int argc, char* argv[])
 {
-
+	
 	return 0;
 }
